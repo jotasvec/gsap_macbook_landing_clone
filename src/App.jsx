950f97c87d4ebe283780ddea8d6a0ccc4977075a
jonathan svec <jotasvec@gmail.com>
2025-10-14 15:29:32 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from './components/NavBar'
+
+const App = () => {
+  return (
+    <>
+        <NavBar />
+        <h1 className=''>hello, Apple macbook pro to the world</h1>
+    </>
+  )
+}
+
+export default App
